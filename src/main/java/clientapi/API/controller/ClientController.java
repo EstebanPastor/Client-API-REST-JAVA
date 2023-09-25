ca39d37f547e6a934fe9ebfbb737f4f0b828df44
EstebanPastor <estebanandrespastor@gmail.com>
2023-09-25 18:49:23 -0300

    add http status codes

--- src/main/java/clientapi/API/controller/ClientController.java
+++ src/main/java/clientapi/API/controller/ClientController.java
@@ -2,6 +2,7 @@ package clientapi.API.controller;
 import clientapi.API.model.entity.Client;
 import clientapi.API.service.IClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,22 +13,26 @@ public class ClientController {
     private IClient clientService;
 
     @PostMapping("client")
+    @ResponseStatus(HttpStatus.CREATED)
     public Client create(@RequestBody Client client) {
         return clientService.save(client);
     }
 
     @PutMapping("client")
+    @ResponseStatus(HttpStatus.CREATED)
     public Client update(@RequestBody Client client) {
         return clientService.save(client);
     }
 
     @DeleteMapping("client/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Integer id) {
         Client clientDelete = clientService.findById(id);
         clientService.delete(clientDelete);
     }
 
     @GetMapping("client/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Client showById(@PathVariable Integer id) {
         return clientService.findById(id);
 
