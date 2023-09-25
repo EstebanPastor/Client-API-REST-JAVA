56829ed06e45e60c6a30b64be73e9abe3c3ba5c1
EstebanPastor <estebanandrespastor@gmail.com>
2023-09-25 19:07:48 -0300

    add a custom message to the http verb delete

--- src/main/java/clientapi/API/controller/ClientController.java
+++ src/main/java/clientapi/API/controller/ClientController.java
@@ -2,8 +2,14 @@ package clientapi.API.controller;
 import clientapi.API.model.entity.Client;
 import clientapi.API.service.IClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -25,10 +31,22 @@ public class ClientController {
     }
 
     @DeleteMapping("client/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Integer id) {
-        Client clientDelete = clientService.findById(id);
-        clientService.delete(clientDelete);
+
+    public ResponseEntity<?> delete(@PathVariable Integer id) {
+        Map<String, Object> response = new HashMap<>();
+
+        try {
+            Client clientDelete = clientService.findById(id);
+            clientService.delete(clientDelete);
+            return new ResponseEntity<>(clientDelete, HttpStatus.NO_CONTENT);
+        } catch (DataAccessException exDt){
+
+            response.put("message", exDt.getMessage());
+            response.put("client", null);
+
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 
     @GetMapping("client/{id}")
